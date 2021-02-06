4fd2955b33f04aef66dc791969417e07e58943b1
RabinPhaiju <rabinphaiju15@gmail.com>
2021-02-06 16:51:58 +0545

    locking query to auth

--- graphql_yoga_prisma/src/resolvers/Post.js
+++ graphql_yoga_prisma/src/resolvers/Post.js
@@ -1,2 +1,12 @@
-const Post = {};
+import getUserId from "./../utils/getUserId.js";
+
+const Post = {
+  //   post(parent, args, { request }, info) {
+  //     const userId = getUserId(request, false);
+  //     if (userId && userId === parent.id) {
+  //       return parent.email;
+  //     }
+  //     return null;
+  //   },
+};
 export { Post as default };
--- graphql_yoga_prisma/src/resolvers/Query.js
+++ graphql_yoga_prisma/src/resolvers/Query.js
@@ -1,3 +1,5 @@
+import getUserId from "./../utils/getUserId.js";
+
 const Query = {
   async users(parent, args, { prisma }, info) {
     if (!args.query) {
@@ -28,9 +30,27 @@ const Query = {
       });
     }
   },
+  async user(parent, args, { prisma }, info) {},
+  async me(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request);
+
+    const [user] = await prisma.user.findMany({
+      where: {
+        id: userId,
+      },
+      include: {
+        posts: true,
+        comments: true,
+      },
+    });
+    return user;
+  },
   async posts(parent, args, { prisma }, info) {
     if (!args.query) {
       return await prisma.post.findMany({
+        where: {
+          published: true,
+        },
         include: {
           comments: true,
           author: true,
@@ -39,6 +59,7 @@ const Query = {
     } else {
       return await prisma.post.findMany({
         where: {
+          published: true,
           OR: [
             {
               title: {
@@ -52,6 +73,59 @@ const Query = {
           author: true,
         },
       });
+    }
+  },
+  async myPosts(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request);
+    if (!args.query) {
+      return await prisma.post.findMany({
+        where: {
+          authorId: userId,
+        },
+        include: {
+          comments: true,
+          author: true,
+        },
+      });
+    } else {
+      return await prisma.post.findMany({
+        where: {
+          authorId: userId,
+          OR: [
+            {
+              title: {
+                contains: args.query,
+              },
+            },
+          ],
+        },
+        include: {
+          comments: true,
+          author: true,
+        },
+      });
+    }
+  },
+  async post(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request, false);
+
+    const [post] = await prisma.post.findMany({
+      where: {
+        id: args.id,
+        OR: [
+          {
+            published: true,
+          },
+          {
+            authorId: userId,
+          },
+        ],
+      },
+    });
+    if (post) {
+      return post;
+    } else {
+      throw new Error("Post not found.");
     }
   },
   async comments(parent, args, { prisma }, info) {
